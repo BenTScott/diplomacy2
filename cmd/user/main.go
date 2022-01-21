5b39d217094e0ece63b4dd6bddb9d973b5b74d7e
Ben Scott <bentysonscott@hotmail.co.uk>
2022-01-21 13:02:57 +0000

    Add not found handler to test

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -14,11 +14,19 @@ func init() {
 	r = httprouter.New()
 	r.GET("/user", handleUser)
 
+	r.NotFound = NotFoundHandler{}
+}
+
+type NotFoundHandler struct{}
+
+func (_ NotFoundHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(*r)
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func handleUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Println(*r)
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func main() {
