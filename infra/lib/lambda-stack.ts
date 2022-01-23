f36cd18e92458aba650c3a98ee72c1cda623bdf5
Ben Scott <bentysonscott@hotmail.co.uk>
2022-01-23 19:09:14 +0000

    Add test function

--- infra/lib/lambda-stack.ts
+++ infra/lib/lambda-stack.ts
@@ -1,8 +1,9 @@
 import {RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {IFunction} from "aws-cdk-lib/aws-lambda";
+import {DockerImageCode, DockerImageFunction, IFunction} from "aws-cdk-lib/aws-lambda";
 import {capitalize} from "./utils";
 import {Repository} from "aws-cdk-lib/aws-ecr";
+import * as path from "path";
 
 export interface LambdaStackProps extends StackProps {
   command: string
@@ -14,15 +15,15 @@ export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
 
-    new Repository(this, capitalize(props.command) + 'Repository', {
+    const repo = new Repository(this, capitalize(props.command) + 'Repository', {
       lifecycleRules: [ { maxImageCount: 10 } ],
       removalPolicy: RemovalPolicy.DESTROY
     });
 
-    // this.function = new DockerImageFunction(this, capitalize(props.command) + 'Function', {
-    //   code: DockerImageCode.fromImageAsset(path.join(__dirname, '../../'), {
-    //     buildArgs: { "cmd": props.command }
-    //   }),
-    // });
+    this.function = new DockerImageFunction(this, capitalize(props.command) + 'Function', {
+      code: DockerImageCode.fromEcr(repo),
+    });
+
+    //buildArgs: { "cmd": props.command }
   }
 }
