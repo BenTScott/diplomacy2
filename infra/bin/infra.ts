3938269e5f6de603dba8a7917e3ac4f9185d7e94
Ben Scott <bentysonscott@hotmail.co.uk>
2022-01-23 17:54:20 +0000

    Remove old pipeline stack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { PipelineStack } from '../lib/pipeline-stack';
 import {CodePipelineStack} from "../lib/codepipeline-stack";
 
 const app = new cdk.App();
-new PipelineStack(app, 'DiplomacyPipelineStack');
 new CodePipelineStack(app, 'DiplomacyCodePipelineStack')
