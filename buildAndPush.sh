b1002c8ae8fe121398d6d8d5153909974a60ede7
Ben Scott <bentysonscott@hotmail.co.uk>
2022-01-25 13:59:01 +0000

    Parallelize docker build script

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -3,15 +3,21 @@
 echo "Found env variable: $LAMBDAS"
 set -e
 
-IFS=';'
-for lambda in $LAMBDAS
-do
+buildAndPush() {
   IFS='|'
-  echo "Building $lambda"
-  read cmd ecr <<< $lambda
+  echo "Building $1"
+  read cmd ecr <<< $1
   echo "Command: $cmd"
   echo "Repo: $ecr"
   docker build . --build-arg cmd=$cmd -t $ecr:$CODEBUILD_RESOLVED_SOURCE_VERSION
   docker tag $ecr:$CODEBUILD_RESOLVED_SOURCE_VERSION $ecr:latest
   docker push $ecr --all-tags
+}
+
+IFS=';'
+for lambda in $LAMBDAS
+do
+  buildAndPush $lambda &
 done
+
+wait
